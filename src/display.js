dcc4c2ce2997b0f21b45b8f438e568bab0aec99c
Conor Kay <conorkay1@gmail.com>
2024-02-08 15:33:00 -0500

    Style form

--- src/display.js
+++ src/display.js
@@ -3,13 +3,13 @@ export const displayController = (function () {
     console.log('renderToDos');
   }
 
-  // Enables the new task form and overlay
+  // Enables form and overlay
   function formOn(form) {
     ovrly.style.display = 'block';
     form.style.display = 'block';
   }
 
-  // Disables the new task form and overlay
+  // Disables form and overlay
   function formOff(form) {
     ovrly.style.display = 'none';
     form.style.display = 'none';
